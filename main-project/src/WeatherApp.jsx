16b113986ebc83169c5c2f0be4015dc2f9dd699c
Krishna <gopikrishna7511@gmail.com>
2025-06-12 18:24:12 +0530

    save changes in name

--- main-project/src/WeatherApp.jsx
+++ main-project/src/WeatherApp.jsx
@@ -18,7 +18,7 @@ export default function WeatherApp() {
   };
   return (
     <div style={{ textAlign: "center" }}>
-      <h2>Weather App By &copy; Prajwal Gandhi</h2>
+      <h2>Weather App By &copy; Gopi Krishna</h2>
       <SearchBox updateInfo={updateInfo}/>
       <InfoBox info={weatherInfo} />
     </div>
